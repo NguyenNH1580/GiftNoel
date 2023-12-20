71194c0ef19b39736a892d5aabdc9883543a1c23
NguyenNH1580 <hoangnguyenht1508@gmail.com>
2023-12-20 14:37:01 +0700

    [feat] replace icon snow to icon heart

--- snow.js
+++ snow.js
@@ -1,17 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
     const numberOfSnowflakes = 50;
+    const snowflakes = [];
 
     for (let i = 0; i < numberOfSnowflakes; i++) {
         createSnowflake();
     }
 
-    function createSnowflake() {
-        var flashCard = document.querySelector('.cards');
+    const heartCheckbox = document.querySelector('.heart__checkbox');
+    let isHeartMode = false;
 
+    heartCheckbox.addEventListener('click', function () {
+        if (!isHeartMode) {
+            // Disable the checkbox to prevent additional clicks during the animation
+            heartCheckbox.disabled = true;
+
+            isHeartMode = true;
+
+            // Replace snowflakes with pink heart icons
+            replaceWithHearts();
+
+            // Set a timer to switch back to snowflakes after 10 seconds
+            setTimeout(() => {
+                isHeartMode = false;
+
+                // Enable the checkbox after the animation is complete
+                replaceWithSnowflakes();
+            }, 10000);
+        }
+    });
+
+    function createSnowflake() {
         const snowflake = document.createElement("div");
         snowflake.className = "snowflake";
-        snowflake.innerHTML = "&#10052;"; // You can change this to any snowflake symbol
+        snowflake.innerHTML = "&#10052;";
         document.body.appendChild(snowflake);
+        snowflakes.push(snowflake);
 
         const initialX = Math.random() * window.innerWidth;
         const initialY = Math.random() * window.innerHeight;
@@ -29,5 +52,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 snowflake.style.top = "0px";
             }
         }, 1000 / 60);
+    }
+
+    function replaceWithHearts() {
+        snowflakes.forEach(snowflake => {
+            snowflake.innerHTML = "&#x1F497;"; // Pink heart icon
+            snowflake.style.color = "#FF69B4"; // Pink color
+        });
+    }
+
+    function replaceWithSnowflakes() {
+        snowflakes.forEach(snowflake => {
+            snowflake.innerHTML = "&#10052;"; // Snowflake icon
+            snowflake.style.color = "#ffffff"; // Reset color to white
+        });
     }
 });
